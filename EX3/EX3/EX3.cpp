beb2e09a7b623b3d533045680d9e4f0f1607727b
Andrey <89473462+Arty525@users.noreply.github.com>
2022-02-07 12:10:22 +0300

    Add files via upload

---
+++ EX3/EX3/EX3.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+double calculation(double fn, double sn, char sign) {
+	if (sign == '+') {
+		return fn + sn;
+	}
+	else if (sign == '-') {
+		return fn - sn;
+	}
+	else if (sign == '/') {
+		return fn / sn;
+	}
+	else if (sign == '*') {
+		return fn * sn;
+	}
+}
+
+int main() {
+	string in;
+	
+	string fnumber;
+	string snumber;
+	char sign;
+
+	cout << "Enter your mathematical expression: ";
+	cin >> in;
+
+	for (int i = 0; in[i] != '+' && in[i] != '-' && in[i] != '/' && in[i] != '*'; ++i) {
+		fnumber += in[i];
+		if (in[i + 1] == '+' || in[i + 1] == '-' || in[i + 1] == '/' || in[i + 1] == '*') {
+			sign = in[i + 1];
+			for (int j = i+2; j < in.length(); ++j) {
+				snumber += in[j];
+			}
+		}
+	}
+
+	double fn = stod(fnumber);
+	double sn = stod(snumber);
+
+	cout << calculation(fn, sn, sign);
+
+	return 0;
+}
