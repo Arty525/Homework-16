faf76bdf4a81a4f81e24e897b8631e76a2f9c46f
Andrey <89473462+Arty525@users.noreply.github.com>
2022-02-07 10:42:09 +0300

    Add files via upload

---
+++ EX2/EX2/EX2.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+	double number;
+	string n;
+
+	cout << "Enter whole part: ";
+	cin >> n;
+	number = stod(n + ".0");
+	cout << "Enter fraction: ";
+	cin >> n;
+	number += stod("0." + n);
+	
+	cout << number;	
+}
