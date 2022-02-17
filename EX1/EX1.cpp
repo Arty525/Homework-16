8e0528b24821081abb661073a72eb252ebaa2d81
Andrey <89473462+Arty525@users.noreply.github.com>
2022-02-17 16:40:25 +0300

    Add files via upload

---
+++ EX1/EX1.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <string>
+#include <cstdio>
+using namespace std;
+
+bool compare(float speed, float current, float different, float epsilon) {
+	return (speed + epsilon >= speed) && (speed - epsilon <= speed);
+}
+
+int main() {
+	char spd[100];
+	float different = 0.f;
+	float current = 0.f;
+	float epsilon = 0.01f;
+	float speed = 0.f;
+	do {
+		sprintf(spd, "Current speed: %.1f km/h", current);
+		cout << spd << endl;
+		cout << "Input different: ";
+		cin >> different;
+		if (compare(speed = current + different, current, different, epsilon)) {
+			speed = current + different;
+			if (speed >= 150.f) {
+				speed = 150.f;
+				cout << "You have reached the maximum speed." << endl;
+			}
+			current = speed;
+			sprintf(spd, "New speed: %.1f km/h", speed);
+			cout << spd << endl << endl;
+		}
+		else {
+			cout << "Value out of range." << endl;
+		}
+	} while (speed > 0);
+}
